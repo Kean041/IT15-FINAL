65febb4d9ec0e5bb6069855924ccc7a141ee218f
eddiegeeeez <e.garan.548856@umindanao.edu.ph>
2026-04-12 15:44:57 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinSight.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
